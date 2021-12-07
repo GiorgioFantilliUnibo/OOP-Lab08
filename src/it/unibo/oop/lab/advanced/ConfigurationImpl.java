78977b5ef747d1d85853c2fbc425c67392ed3d76
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-07 15:40:52 +0100

    feat: create ConfigurationImpl class

---
+++ src/it/unibo/oop/lab/advanced/ConfigurationImpl.java
@@ -0,0 +1,17 @@
+package it.unibo.oop.lab.advanced;
+
+/**
+ * {@inheritDoc}
+ */
+public class ConfigurationImpl implements Configuration {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isConsistent() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
