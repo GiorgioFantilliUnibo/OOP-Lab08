382986c7ca2b47add9affc56780353a48d6398db
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-07 15:53:25 +0100

    feat: implement constructor

--- src/it/unibo/oop/lab/advanced/ConfigurationImpl.java
+++ src/it/unibo/oop/lab/advanced/ConfigurationImpl.java
@@ -5,6 +5,32 @@ package it.unibo.oop.lab.advanced;
  */
 public class ConfigurationImpl implements Configuration {
 
+    private static final int DEFAULT_MIN = 0;
+    private static final int DEFAULT_MAX = 100;
+    private static final int DEFAULT_ATTEMPTS = 10;
+
+    private final int min;
+    private final int max;
+    private final int attempts;
+    private final boolean isConsistent;
+
+    /**
+     * Build a new {@link ConfigurationImpl}.
+     * 
+     * @param min
+     *          lower value assumed by the number
+     * @param max
+     *          greater value assumed by the number
+     * @param attempts
+     *          maximum number of possible attempts
+     */
+    public ConfigurationImpl(final int min, final int max, final int attempts) {
+        this.min = min;
+        this.max = max;
+        this.attempts = attempts;
+        this.setConsistency();
+    }
+
     /**
      * {@inheritDoc}
      */
