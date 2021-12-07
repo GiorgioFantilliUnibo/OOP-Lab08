116c793c9613d35e2ee044c9ccbcfb783ce1b252
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-07 02:01:51 +0100

    feat: created ControllerImpl class

---
+++ src/it/unibo/oop/lab/mvc/ControllerImpl.java
@@ -0,0 +1,44 @@
+package it.unibo.oop.lab.mvc;
+
+/**
+ * {@inheritDoc}
+ */
+public class ControllerImpl implements Controller {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setNextString(final String s) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getNextString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getPrintHistory() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void print() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
