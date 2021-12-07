50c72e7ff86733fe0d5237fcfe77dba1403aba0e
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-07 21:56:57 +0100

    feat: create PrintView class

---
+++ src/it/unibo/oop/lab/advanced/PrintView.java
@@ -0,0 +1,44 @@
+package it.unibo.oop.lab.advanced;
+
+/**
+ * Output only view.
+ */
+public class PrintView implements DrawNumberView {
+
+    @Override
+    public void setObserver(DrawNumberViewObserver observer) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void numberIncorrect() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void result(DrawResult res) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void limitsReached() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void displayError(String message) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
