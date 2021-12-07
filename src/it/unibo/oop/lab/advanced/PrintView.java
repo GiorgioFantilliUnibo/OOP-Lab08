b4475860797f33b09161099484c82a11dd249f65
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-07 22:20:29 +0100

    feat: implement PrintView

--- src/it/unibo/oop/lab/advanced/PrintView.java
+++ src/it/unibo/oop/lab/advanced/PrintView.java
@@ -1,44 +1,92 @@
 package it.unibo.oop.lab.advanced;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 /**
  * Output only view.
  */
 public class PrintView implements DrawNumberView {
 
-    @Override
-    public void setObserver(DrawNumberViewObserver observer) {
-        // TODO Auto-generated method stub
+    private final PrintStream stream;
 
+    /**
+     * Builds a {@link PrintView} that writes on a given stream.
+     * 
+     * @param stream 
+     *          the {@link PrintView} to write to
+     */
+    public PrintView(final PrintStream stream) {
+        this.stream = stream;
     }
 
-    @Override
-    public void start() {
-        // TODO Auto-generated method stub
-
+    /**
+     * Builds a {@link PrintView} that writes on a indicated file.
+     * 
+     * @param path 
+     *          the file path
+     * @throws FileNotFoundException 
+     */
+    public PrintView(final String path) throws FileNotFoundException {
+        this.stream = new PrintStream(new FileOutputStream(new File(path)));
     }
 
+    /**
+     * Do nothing, it is a output-only view.
+     */
+    @Override
+    public void setObserver(final DrawNumberViewObserver observer) { }
+
+    /**
+     * Do nothing, it is a output-only view.
+     */
+    @Override
+    public void start() { }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void numberIncorrect() {
-        // TODO Auto-generated method stub
-
+        this.stream.print("Number out of bound");
     }
 
+    /**
+     * Print the result of the last attempt.
+     * 
+     * @param res
+     *          result of the last attempt
+     */
     @Override
-    public void result(DrawResult res) {
-        // TODO Auto-generated method stub
-
+    public void result(final DrawResult res) {
+        switch (res) {
+        case YOURS_HIGH:
+        case YOURS_LOW:
+        case YOU_WON:
+            this.stream.print(res.getDescription());
+            break;
+        default:
+            this.stream.print("Unexpected result: " + res);
+            break;
+        }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void limitsReached() {
-        // TODO Auto-generated method stub
-
+        this.stream.print("You lost! Reached the maximum number of attempts.");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void displayError(String message) {
-        // TODO Auto-generated method stub
-
+    public void displayError(final String message) {
+        this.stream.print(message);
     }
 
 }
