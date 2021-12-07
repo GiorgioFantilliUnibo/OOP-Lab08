f4494dc61570f62fbc815a1d405541c683723333
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-07 16:18:59 +0100

    feat: implement constructor by Configuration class

--- src/it/unibo/oop/lab/advanced/DrawNumberImpl.java
+++ src/it/unibo/oop/lab/advanced/DrawNumberImpl.java
@@ -15,17 +15,15 @@ public final class DrawNumberImpl implements DrawNumber {
     private final Random random = new Random();
 
     /**
-     * @param min
-     *            minimum number
-     * @param max
-     *            maximum number
-     * @param attempts
-     *            the maximum number of attempts
+     * Build a new {@link DrawNumberImpl}.
+     * 
+     * @param config
+     *          configuration of the game
      */
-    public DrawNumberImpl(final int min, final int max, final int attempts) {
-        this.min = min;
-        this.max = max;
-        this.attempts = attempts;
+    public DrawNumberImpl(final Configuration config) {
+        this.min = config.getMin();
+        this.max = config.getMax();
+        this.attempts = config.getAttempts();
         this.reset();
     }
 
